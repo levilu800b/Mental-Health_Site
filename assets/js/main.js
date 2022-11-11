c33cde5281e40aa7bf4ccfd55ae1088ae3926bc0
levilu800b <levilu800b@gmail.com>
2022-11-11 14:00:49 +0000

    Main Js

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+/*=============== SHOW MENU ===============*/
+// Write your code here
+
+/*=============== REMOVE MENU MOBILE ===============*/
+// Write your code here
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+// Write your code here
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+// Write your code here
+
+/*=============== SHOW SCROLL UP ===============*/
+// Write your code here
+
+/*=============== DARK LIGHT THEME ===============*/
+// Write your code here
