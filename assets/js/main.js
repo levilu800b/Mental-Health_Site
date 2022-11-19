a77847cfbbbf9e7be8a4478f976ae7070cfa5a0f
levilu800b <levilu800b@gmail.com>
2022-11-19 10:54:20 +0000

    dark mode

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,37 @@
-/*=============== SHOW MENU ===============*/
-// Write your code here
-
-/*=============== REMOVE MENU MOBILE ===============*/
-// Write your code here
-
-/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-// Write your code here
-
-/*=============== CHANGE BACKGROUND HEADER ===============*/
-// Write your code here
-
-/*=============== SHOW SCROLL UP ===============*/
-// Write your code here
-
 /*=============== DARK LIGHT THEME ===============*/
-// Write your code here
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+const iconTheme = 'bx-toggle-right';
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () =>
+	document.body.classList.contains(darkTheme) ? 'dark' : 'light';
+const getCurrentIcon = () =>
+	themeButton.classList.contains(iconTheme)
+		? 'bx-toggle-left'
+		: 'bx-toggle-right';
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+	// If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+	document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](
+		darkTheme,
+	);
+	themeButton.classList[selectedIcon === 'bx-toggle-left' ? 'add' : 'remove'](
+		iconTheme,
+	);
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+	// Add or remove the dark / icon theme
+	document.body.classList.toggle(darkTheme);
+	themeButton.classList.toggle(iconTheme);
+	// We save the theme and the current icon that the user chose
+	localStorage.setItem('selected-theme', getCurrentTheme());
+	localStorage.setItem('selected-icon', getCurrentIcon());
+});
